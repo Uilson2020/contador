32f0aa61302a35f85f80916138db1bafd678f312
Treinamento <Treinamento@RD723932.raiadrogasil.com>
2020-02-27 15:29:04 -0300

    contador

---
+++ contador/src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Contador from './componentes/Contador'
+import "bootstrap/dist/css/bootstrap.min.css";
+import './styles/global.css'
+
+
+
+class App extends Component {
+  render() {
+    return <Contador/>
+  }
+ 
+}
+export default App
